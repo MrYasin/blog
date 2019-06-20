7f51863535045c163e5eaef4122e0e3085996aa9
MrYasin <mryasincengiz@gmail.com>
2019-06-20 15:26:21 +0300

    Fix for add_article

--- blog.py
+++ blog.py
@@ -219,28 +219,26 @@ def article(id):
 
 ### ADD ARTICLE
 
-@app.route("/addarticle",methods = ["GET","POST"])
-def addarticle():
+@app.route("/addarticle", methods = ["GET", "POST"])
+def add_article():
+
     form = Article(request.form)
     if request.method == "POST" and form.validate():
+
         title = form.title.data
         content = form.content.data
-
+        
         cursor = mysql.connection.cursor()
-
-        sorgu = "Insert into articles(title,author,content) VALUES(%s,%s,%s)"
-
-        cursor.execute(sorgu,(title,session["username"],content))
+        inquiry = "INSERT INTO articles(title,author,content) VALUES(%s,%s,%s)"
+        cursor.execute(inquiry,(title, session["username"], content))
 
         mysql.connection.commit()
-
         cursor.close()
 
-        flash("Makale Başarıyla Eklendi","success")
-
+        flash("Article added successfuly.","success")
         return redirect(url_for("dashboard"))
-
-    return render_template("addarticle.html",form = form)
+    
+    return render_template("addarticle.html", form = form)
 
 
 ### DELETE ARTICLE
